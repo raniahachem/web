6f550b9c37b8a8cd190fdf99950545861889765a
ons198 <92670143+ons198@users.noreply.github.com>
2023-04-28 10:40:59 +0100

    Add files via upload

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,8 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    "TwigExtraBundle" => Twig\Extra\TwigExtraBundle\TwigExtraBundle::class,
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
